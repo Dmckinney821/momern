2755f371ac78eb53358396a51f7bd954b625b7be
Dan McKinney <3dhdaniel@gmail.com>
2019-11-29 11:56:38 -0500

    thanksgiving thanks

--- src/App.js
+++ src/App.js
@@ -1,32 +1,70 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom'
 import Users from './user/pages/Users'
 import NewPlace from './places/pages/NewPlace'
 import UserPlaces from './places/pages/UserPlaces'
+import Auth from './user/pages/Auth'
 import MainNavigation from './shared/components/Navigation/MainNavigation'
+
+import { AuthContext } from './shared/context/auth-context'
+
 import UpdatePlace from './places/pages/UpdatePlace'
 const  App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  
+  const login = useCallback(() => {
+    setIsLoggedIn(true)
+  }, [])
+  const logout = useCallback(() => {
+    setIsLoggedIn(false)
+  }, [])
+
+  let routes;
+
+  if (isLoggedIn) {
+    routes = (
+      <Switch>
+        <Route path='/' exact>
+          <Users /> 
+        </Route>
+        <Route path='/:userId/places'>
+          <UserPlaces exact />
+        </Route>
+        <Route path='/places/new' exact>
+          <NewPlace /> 
+        </Route>
+        <Route path='/places/:placeId'>
+          <UpdatePlace />
+        </Route>
+        <Redirect to ='/' />
+      </Switch>
+    );
+  } else {
+    routes = (
+      <Switch>
+        <Route path='/' exact>
+          <Users /> 
+        </Route>
+        <Route path='/:userId/places'>
+          <UserPlaces exact />
+        </Route>
+        <Route path='/auth'>
+          <Auth />
+        </Route>
+        <Redirect to ='/auth' /> 
+      </Switch>
+    )
+  }
+
   return (
+    <AuthContext.Provider value={{isLoggedIn: isLoggedIn, login: login, logout: logout}}>
     <Router>
       <MainNavigation />
       <main>
-      <Switch>
-      <Route path='/' exact>
-        <Users /> 
-      </Route>
-      <Route path='/:userId/places'>
-        <UserPlaces exact />
-      </Route>
-      <Route path='/places/new' exact>
-        <NewPlace /> 
-      </Route>
-      <Route path='/places/:placeId'>
-    <UpdatePlace />
-      </Route>
-      <Redirect to='/'/>
-      </Switch>
+        {routes}
       </main>
     </Router>
+    </AuthContext.Provider>
   )
 }
 
--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -2,7 +2,6 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import PlaceList from '../components/PlaceList'
-
 const DUMMY_PLACES = [
     {
         id: 'p1',
@@ -18,7 +17,7 @@ const DUMMY_PLACES = [
     },
     {
         id: 'p2',
-        title: 'Empire State Building',
+        title: 'Emp. State Building',
         description: 'One of the most famous buildings',
         imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
         address: '20 W 34th St, New York, NY 10001',
